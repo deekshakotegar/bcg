16a8add4572ec1d17701177b2b480843e9e4e0c2
deekshakotegar <deeksha.kotegar@gmail.com>
2019-06-05 17:37:44 +0530

    fixes for responsive header

--- src/Components/Menu.tsx
+++ src/Components/Menu.tsx
@@ -3,58 +3,64 @@ import './Menu.scss';
 
 interface IResMenustate {
     showMenu: boolean;
+    showClass: string;
 }
 
 export default class Menu extends React.Component<{}, IResMenustate> {
     constructor(props: any) {
         super(props)
         this.state = {
+            showClass: '',
             showMenu: true
         }
     }
     public render() {
         return (
             <div className='menuBlk'>
-
-                <input type="checkbox" />
-                <span />
-                <span />
-                <span />
                 {this.state.showMenu ?
-                    <ul className='menu' id='menu'>
-                        <li>
-                            <a href='#WhoWeAre' onClick={this.res}>
-                                who we are
+                    <React.Fragment>
+                        <input type="checkbox" />
+                        <span />
+                        <span />
+                        <span />
+
+                        <ul className='menu' id='menu'>
+                            <li>
+                                <a href='#WhoWeAre' onClick={this.resMenu} className={this.state.showClass && 'active'}>
+                                    who we are
                     </a>
-                        </li>
-                        <li>
-                            <a href='#WhatWeDo' onClick={this.res}>
-                                what we do
+                            </li>
+                            <li>
+                                <a href='#WhatWeDo' onClick={this.resMenu}>
+                                    what we do
                     </a>
-                        </li>
-                        <li>
-                            <a href='#OurWork' onClick={this.res}>
-                                our work
+                            </li>
+                            <li>
+                                <a href='#OurWork' onClick={this.resMenu}>
+                                    our work
                     </a>
-                        </li>
-                        <li>
-                            <a href='#GetInTouch' onClick={this.res}>
-                                get in touch
+                            </li>
+                            <li>
+                                <a href='#GetInTouch' onClick={this.resMenu}>
+                                    get in touch
                     </a>
-                        </li>
-                        <li>
-                            <a href='#blog' onClick={this.res}>
-                                Blog
+                            </li>
+                            <li>
+                                <a href='#Blog' onClick={this.resMenu}>
+                                    Blog
                     </a>
-                        </li>
-                    </ul> : false}
+                            </li>
+                        </ul></React.Fragment> : false}
             </div>
         )
     }
 
-    public res = () => {
+    public resMenu = () => {
         if (window.innerWidth <= 768) {
-            this.setState((prevState) => ({ showMenu: !prevState.showMenu }))
+            this.setState(() => ({ showMenu: !this.state.showMenu }))
+            setTimeout(() => {
+                this.setState(() => ({ showMenu: true }))
+            }, 500)
         }
     }
 }
